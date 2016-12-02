c8272ee552417964991c36196b96bc583bc89394
oussem76 <oussem93@hotmail.fr>
2016-12-02 04:37:22 +0100

    Modification ContactDAO

--- TutorialStruts/src/miage/domaine/ContactDAO.java
+++ TutorialStruts/src/miage/domaine/ContactDAO.java
@@ -1,42 +1,140 @@
 package miage.domaine;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
+import java.sql.Statement;
 
 public class ContactDAO {
 	private final static String RESOURCE_JDBC = "java:comp/env/jdbc/dsMyDB";
+	
 
 	
+	public void addGroup(String nameGroup){
+		
+		final String driver= "com.mysql.jdbc.Driver";
+		final String url="jdbc:mysql://localhost/weecop_miage_nanterre";
+		final String login="root";
+		final String mdp="root";
+		Statement stmt = null;
+		Connection connect = null;
+		
+		
+		String requete = "insert into 'group' (name) values('"+nameGroup+"')";
+		
+		try{
+			Class.forName(driver);
+			connect= DriverManager.getConnection(url,login,mdp);
+			stmt=connect.createStatement();
+			stmt.executeUpdate(requete); // Exécute la requête
+
+			//System.out.println("Insertion : exécuté");
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+			System.out.println("Echec de la connexion");
+		}finally{
+			try{
+
+				if(stmt !=null){
+					stmt.close();
+				}
+				if(connect !=null){
+					connect.close();
+				}
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+			
+		}
+
+	}
 	
-	public void addContact(Long id, String firstName, String lastName, 
-					String email) {
-		try {
-			final Context context = new InitialContext();
-			final DataSource  ds = (DataSource) context.lookup(RESOURCE_JDBC);
-			final Connection cnx = ds.getConnection();
-			final PreparedStatement ps = 
-					cnx.prepareStatement("INSERT INTO CONTACT(FIRSTNAME, LASTNAME, EMAIL) VALUES(?, ?, ?)");
-			
-			ps.setString(1, firstName);
-			ps.setString(2, lastName);
-			ps.setString(3, email);
-			
-			ps.executeUpdate();			
-		} catch (NamingException e) {
-			// TODO Auto-generated catch block
+	public void afficheGroup(){
+		final String driver= "com.mysql.jdbc.Driver";
+		final String url="jdbc:mysql://localhost/weecop_miage_nanterre";
+		final String login="root";
+		final String mdp="root";
+		Statement stmt = null;
+		Connection connect = null;
+		
+		
+		String requete = "select * from group";
+		
+		try{
+			Class.forName(driver);
+			connect= DriverManager.getConnection(url,login,mdp);
+			stmt=connect.createStatement();
+			stmt.executeUpdate(requete); // Exécute la requête
+
+			//System.out.println("select : exécuté");
+
+		}catch(Exception e)
+		{
 			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Echec de la connexion");
+		}finally{
+			try{
+
+				if(stmt !=null){
+					stmt.close();
+				}
+				if(connect !=null){
+					connect.close();
+				}
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+	
+	public boolean deleteGroup(String idGroup)
+	{
+		boolean result = false;
+		
+		String driver= "com.mysql.jdbc.Driver";
+		String url="jdbc:mysql://localhost/weecop_miage_nanterre ";
+		String login="root";
+		String mdp="root";
+		String requete = "DELETE from group where id='" +idGroup+"'";
+		
+		System.out.println("La requete suppression = " +requete);
+		
+		Statement stmt = null;
+		Connection connect = null;
+		
+		try{
+			Class.forName(driver);
+			connect= DriverManager.getConnection(url,login,mdp);
+			stmt=connect.createStatement();
+			// Exécute la requête
+			stmt.executeUpdate(requete); 
+			result = true;
+			
+			//System.out.println("Passage Supression");
+		}catch(Exception e)
+		{
 			e.printStackTrace();
+			System.out.println("Echec de la connexion");
+		}finally{
+			try{
+
+				if(stmt !=null){
+					stmt.close();
+				}
+				if(connect !=null){
+					connect.close();
+				}
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+			
 		}
 		
-		
+		return result;
 	}
 
 }
